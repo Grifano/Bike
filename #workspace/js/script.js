fc1e8b6931a2ad4721447f4ae8e0d31c18366092
Grifano <sorlenk@gmail.com>
2020-09-06 14:18:55 +0200

    before push

---
+++ #workspace/js/script.js
@@ -0,0 +1,11 @@
+// === #Burger icon click action ===
+function burgerIcon() {
+  document.body.classList.toggle('_lock')
+  var menuShow = document.getElementById("showMenu");
+  menuShow.classList.toggle('_show');
+  var burgerClose = document.getElementById('burgerClose');
+  burgerClose.classList.toggle('_active');
+}
+// === /Burger icon click action ===
+
+//****************************************
